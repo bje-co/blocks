07b805ac8cb8d6c9c856accac474e9669a60e11e
Brian Espinosa <b@bje.co>
2025-07-12 14:00:53 -0700

    fix: Boundaries transpile correctly for nextjs

--- src/components/Boundaries/Boundaries.tsx
+++ src/components/Boundaries/Boundaries.tsx
@@ -6,7 +6,7 @@ import { ErrorBoundary, type FallbackProps } from 'react-error-boundary';
 import DefaultErrorFallback from './ErrorFallback';
 import DefaultSuspenseFallback from './SuspenseFallback';
 
-export interface BoundariesProps { 
+export interface BoundariesProps {
   /**
    * Can provide an ErrorComponent to show as a fallback.
    */
@@ -18,22 +18,29 @@ export interface BoundariesProps {
 
   children?: React.ReactNode;
 }
- 
+
 /**
  * A component that wraps its children in an ErrorBoundary and Suspense.
  * It provides default components for error and suspense fallbacks,
  * which can be overridden by passing custom components as props.
  */
-export const Boundaries = ({
+export function Boundaries({
   ErrorComponent = DefaultErrorFallback,
   SuspenseComponent = DefaultSuspenseFallback,
   children,
-}: BoundariesProps) => {
+}: BoundariesProps) {
   return (
     <Suspense fallback={<SuspenseComponent />}>
-      <ErrorBoundary FallbackComponent={ErrorComponent}>
+      <ErrorBoundary
+        fallbackRender={({ error, resetErrorBoundary }) => (
+          <ErrorComponent
+            error={error}
+            resetErrorBoundary={resetErrorBoundary}
+          />
+        )}
+      >
         {children}
       </ErrorBoundary>
     </Suspense>
   );
-};
+}
